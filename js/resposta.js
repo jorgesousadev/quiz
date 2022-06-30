9c1fe3599e8ef3fabac1c78667ec130f22d60a72
Jorge Sousa <108442763+JorgeSousaDev@users.noreply.github.com>
2022-06-30 15:16:19 -0300

    Add files via upload

---
+++ js/resposta.js
@@ -0,0 +1,211 @@
+var pt = 1000;
+
+function pergunta1(){
+    let alt11 = document.getElementById("alt11").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt11 == true) {
+         document.getElementById("resp1").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp1").textContent = resultado2
+     }
+     
+} 
+
+function pergunta2(){
+    let alt22 = document.getElementById("alt22").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt22 == true) {
+         document.getElementById("resp2").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp2").textContent = resultado2
+     }
+     
+} 
+
+function pergunta3(){
+    let alt33 = document.getElementById("alt33").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt33 == true) {
+         document.getElementById("resp3").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp3").textContent = resultado2
+     }
+     
+} 
+
+function pergunta4(){
+    let alt42 = document.getElementById("alt42").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt42 == true) {
+         document.getElementById("resp4").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp4").textContent = resultado2
+     }
+     
+} 
+
+function pergunta5(){
+    let alt51 = document.getElementById("alt51").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt51 == true) {
+         document.getElementById("resp5").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp5").textContent = resultado2
+     }
+     
+} 
+
+function pergunta6(){
+    let alt62 = document.getElementById("alt62").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt62 == true) {
+         document.getElementById("resp6").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp6").textContent = resultado2
+     }
+     
+} 
+
+function pergunta7(){
+    let alt71 = document.getElementById("alt71").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt71 == true) {
+         document.getElementById("resp7").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp7").textContent = resultado2
+     }
+     
+} 
+
+function pergunta8(){
+    let alt82 = document.getElementById("alt82").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt82 == true) {
+         document.getElementById("resp8").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp8").textContent = resultado2
+     }
+     
+} 
+
+function pergunta9(){
+    let alt92 = document.getElementById("alt92").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt92 == true) {
+         document.getElementById("resp9").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp9").textContent = resultado2
+     }
+     
+} 
+
+function pergunta10(){
+    let alt101 = document.getElementById("alt101").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt101 == true) {
+         document.getElementById("resp10").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp10").textContent = resultado2
+     }
+     
+} 
+
+function pergunta11(){
+    let alt112 = document.getElementById("alt112").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt112 == true) {
+         document.getElementById("resp11").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp11").textContent = resultado2
+     }
+     
+} 
+
+function pergunta12(){
+    let alt121 = document.getElementById("alt121").checked
+    let resultado = "Parabéns!! Acertou!!!"
+    let resultado2 = "Errou!!! Quem sabe na próxima vez!!!"
+ 
+     if (alt121 == true) {
+         document.getElementById("resp12").textContent = resultado
+         document.getElementById("result").textContent = pt 
+         pt = pt + 1000
+     }
+     else
+     {
+        document.getElementById("resp12").textContent = resultado2
+     }
+     
+} 
+
+function reset(){
+    document.getElementById("result").textContent = pt 
+    pt = pt + 1000
+}
+
